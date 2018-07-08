5000604f5df915f6f5164b474ad403b13fa0cd2e
tkl880622 <40885253+tkl880622@users.noreply.github.com>
2018-07-08 11:07:09 +0800

    Update A.java

--- src/aa/bb/A.java
+++ src/aa/bb/A.java
@@ -3,6 +3,7 @@ package aa.bb;
 public class A {
 
     public static void main(String[] args) {
+        System.out.println("ttttttttttttttttttt");System.out.println("ttttttttttttttttttt");System.out.println("ttttttttttttttttttt");System.out.println("ttttttttttttttttttt");
         System.out.println("ttttttttttttttttttt");
     }
 }
